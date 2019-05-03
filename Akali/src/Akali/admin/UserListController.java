6d98abf77a7b0a22d7a8912f6e39d90ca5e222b6
SantiagoHernandezG <SantiagoHernandez2@icloud.com>
2019-05-03 14:08:58 -0500

    Updated the AdminController with file Management

--- Akali/src/Akali/admin/UserListController.java
+++ Akali/src/Akali/admin/UserListController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 
+import java.io.*;
+
 public class UserListController {
 
     @FXML
@@ -19,5 +21,40 @@ public class UserListController {
 
     @FXML
     void handleBackButton(){}
+
+    public boolean createFile(Object u) {
+        boolean confirmed = false;
+        try {
+            FileOutputStream fos = new FileOutputStream("admin.alv", true);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            //Crear file
+            //Consultar existencia
+            //Borrar mijos
+            oos.writeObject(u);
+            oos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return confirmed;
+    }
+
+
+    public boolean consultFile() {
+        boolean confirmed = true;
+
+        try {
+            FileInputStream fis = new FileInputStream("");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return confirmed;
+    }
+
+    public boolean
 
 }
