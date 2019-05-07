a33f0342285b2aaf27829c96573979a458ab7443
TiggerZ97 <hrswfu@hotmail.com>
2019-05-07 07:19:30 -0500

    Translator ongoing added, need help with the buttons

--- Akali/src/Akali/translator/OngoingController.java
+++ Akali/src/Akali/translator/OngoingController.java
@@ -5,7 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import resource.AbstractResource;
 import vistanavigator.VistaNavigator;
+
+import static logic.Browse.getIRes;
 
 public class OngoingController {
     @FXML
@@ -21,6 +25,14 @@ public class OngoingController {
 
     @FXML
     void handleBack() {
+        vistanavigator.VistaNavigator.loadVista(VistaNavigator.COMMON_ADDUSER);
+    }
+    @FXML
+    void initialize() {
+        title.setCellValueFactory(new PropertyValueFactory<AbstractResource, String>("title"));
+        status.setCellValueFactory(new PropertyValueFactory<AbstractResource, String>("status"));
+        buttact.setCellValueFactory(new PropertyValueFactory<AbstractResource, Button>("button"));
+        table.setItems(getIRes());
     }
 
 }
